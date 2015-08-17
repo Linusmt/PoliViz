d040fb362818a1e3da87d9246d39ad80b582befa
Linus Meyer-Teruel <lmeyerte@stanford.edu>
2015-08-17 16:30:34 -0700

    Did some rebae stuff

--- server/db/SQL/SQLController.js
+++ server/db/SQL/SQLController.js
@@ -5,13 +5,8 @@ var getSummaryData = function(req, res, next){
     res.send(results)
   })
 };
-<<<<<<< HEAD
 
 var getSummaryDataByName = function(req, res, next){ 
-=======
-//invidividual candidate data
-var getCandidateData = function(req, res, next){ 
->>>>>>> (feat) prepared the directory for deployment
   var candName = req.body.candName;
   SQLdb.getCandidateFinanceDataByName(candName, function(results){ 
     res.send(results)
